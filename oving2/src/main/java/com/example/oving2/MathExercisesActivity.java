9f7d3bdb460d018f196b32f51664029b742aae5d
Joakim Moe Adolfsen <joakimad@stud.ntnu.no>
2020-08-26 17:55:49 +0200

    Started on part 2

---
+++ oving2/src/main/java/com/example/oving2/MathExercisesActivity.java
@@ -0,0 +1,27 @@
+package com.example.oving2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MathExercisesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_exercises);
+    }
+
+
+
+
+
+    /* Displays result in a toast
+                Context context = getApplicationContext();
+                CharSequence text = "" + randomNum;
+                int duration = Toast.LENGTH_SHORT;
+                Toast resultToast = Toast.makeText(context, text, duration);
+                resultToast.show();
+                */
+
+}
--- oving2/src/main/java/com/example/oving2/RandomNumberActivity.java
+++ oving2/src/main/java/com/example/oving2/RandomNumberActivity.java
@@ -1,12 +1,9 @@
 package com.example.oving2;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
